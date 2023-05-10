cb1954416d38d1dca3db6366032754ddd22af1c5
weber134 <webernadweber13@gmail.com>
2023-05-10 19:02:25 +0800

    no way commit

---
+++ Web410903717/Controllers/testController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410903717.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
