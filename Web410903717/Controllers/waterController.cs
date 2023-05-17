cd1d15b20100628e33203db95657ce9e24cf321b
weber134 <weberandweber13@gmail.com>
2023-05-17 19:48:08 +0800

    第一個作業

---
+++ Web410903717/Controllers/waterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410903717.Controllers
+{
+    public class waterController : Controller
+    {
+        // GET: water
+        public ActionResult Index(string name, float? weight)
+        {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
