3477a3e79049f1d2270b87244c763a4c598623f4
weber134 <weberandweber13@gmail.com>
2023-05-17 20:40:18 +0800

    token 7

---
+++ Web410903717/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410903717.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SignUp(string name, string account, string password)
+        {
+            return View();
+        }
+
+
+    }
+}
